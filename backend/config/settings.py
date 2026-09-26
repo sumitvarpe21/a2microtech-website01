7cb2efe7b0d0f2f7fb39c84344ffcbace96fc2f2
sumitvarpe21 <EMAIL_USED_ON_GITHUB>
2026-09-26 16:38:07 +0530

    Fix Cloudinary image URLs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,9 +135,9 @@ REST_FRAMEWORK = {
     ],
 }
 
-
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
     "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+    "PREFIX": "",
 }
